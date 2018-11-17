26dda39b13024fbb5ac016b0087af982a7876ae0
Andrew Foltz-Morrison <afoltzm@gmail.com>
2018-11-17 11:49:36 -0500

    partial, curry, uncurry, compose

--- src/main/scala/fpscala/chapter2.scala
+++ src/main/scala/fpscala/chapter2.scala
@@ -13,7 +13,7 @@ object Chapter2 {
     }
     go(n, 1)
   }
-
+  // exercise 2.1 (did not solve on my own)
   def fibo(x: Int): Int = {
     @tailrec
     def go(n: Int, prev: Int, cur: Int): Int = {
@@ -81,7 +81,7 @@ object Chapter2 {
     }
     loop(0)
   }
-
+  // exercise 2.2 (solved)
   /* recursively determines whether the given sequence is ordered according to the given fn */
   def isSorted[A](as: Seq[A], ordered: (A, A) => Boolean): Boolean = {
     // strategy: compare the nth and n+1th elements
@@ -92,6 +92,24 @@ object Chapter2 {
       else loop(n + 1)
     }
     loop(0)
+  }
+
+  def partial1[A, B, C](a: A, f: (A,B) => C): B => C = {
+    (b: B) => f(a,b)
+  }
+  // exercise 2.3 (solved)
+  def curry[A, B, C](f: (A,B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a,b)
+  }
+
+  // exercise 2.4 (solved)
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  // exercise 2.5
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
   }
 
 }
