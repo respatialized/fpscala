c932b8a46c40b25a211a2bc1eaee23281e2c84e8
Andrew Foltz-Morrison <afoltzm@gmail.com>
2018-11-17 10:40:23 -0500

    Refactoring into a higher-order function

--- src/main/scala/fpscala/chapter2.scala
+++ src/main/scala/fpscala/chapter2.scala
@@ -1,6 +1,42 @@
 package fpscala
 
+import scala.annotation.tailrec
 
 object Chapter2 {
+  def abs(n: Int): Int = if (n < 0) -n else n
 
+  def factorial(n: Int): Int = {
+    @tailrec
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc
+      else go(n-1, n * acc)
+    }
+    go(n, 1)
+  }
+
+  def fibo(x: Int): Int = {
+    @tailrec
+    def go(n: Int, prev: Int, cur: Int): Int = {
+      if (n == 0) prev
+      else go(n - 1, cur, prev + cur)
+    }
+    go(x, 0, 1)
+  }
+
+  private def formatAbs(x: Int): String = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  // the input of a function can be any type we wish, including
+  // a mapping from one type to another (which is another way of saying function)
+  def formatResult(name: String, n: Int, f: Int => Int): String = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+  }
 }
