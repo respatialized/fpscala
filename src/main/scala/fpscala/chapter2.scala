f1a2bb85848985b0a15de0884f3011ab488ed262
Andrew Foltz-Morrison <afoltzm@gmail.com>
2018-11-17 11:23:19 -0500

    Generalizing functions across types

--- src/main/scala/fpscala/chapter2.scala
+++ src/main/scala/fpscala/chapter2.scala
@@ -39,4 +39,59 @@ object Chapter2 {
     println(formatResult("absolute value", -42, abs))
     println(formatResult("factorial", 7, factorial))
   }
+
+  /* returns the index of the first occurence of key in sequence ss
+   * or -1 if it's not there */
+  def findFirst(ss: Seq[String], key: String): Int = {
+
+    /* this helper function is a good example of thinking clearly about exactly what
+     * you're iterating over in a recursive function. My first instinct would be to
+     * iterate over the elements, but that leaves you without an index, which is the
+     * whole point of writing the function in the first place! instead, the function
+     * is iterating over the indices and using those to compare values */
+    @tailrec
+    def loop(n: Int): Int =
+      if (n >= ss.length) -1
+      else if (ss(n) == key) n
+      else loop(n + 1)
+    loop(0)
+  }
+
+  /* Writing a polymorphic version allows us to abstract over the input type as well as the
+   * method by which we check the elements by putting the boolean logic into an input function */
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    /* I forgot to annotate the return type of the recursive function, which Scala doesn't allow. */
+    @tailrec
+    def loop(n: Int): Int = {
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+    }
+
+    loop(0)
+  }
+
+  // solving the special case before generalizing - determines if it's an ascending list
+  def isSorted(is: Seq[Int]): Boolean = {
+    @tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= is.length - 1) true // nothing left to compare
+      else if (is(n + 1) < is(n)) false
+      else loop(n + 1)
+    }
+    loop(0)
+  }
+
+  /* recursively determines whether the given sequence is ordered according to the given fn */
+  def isSorted[A](as: Seq[A], ordered: (A, A) => Boolean): Boolean = {
+    // strategy: compare the nth and n+1th elements
+    @tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length - 1) true // nothing left to compare
+      else if (!ordered(as(n), as(n + 1))) false
+      else loop(n + 1)
+    }
+    loop(0)
+  }
+
 }
