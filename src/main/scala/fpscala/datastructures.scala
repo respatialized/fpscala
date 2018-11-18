816c64f2d14ddeedad4a6c16f4383c09f8d0aae5
Andrew Foltz-Morrison <afoltzm@gmail.com>
2018-11-18 14:04:45 -0500

    strongly typed, purely functional, tail-recursive list operations

--- src/main/scala/fpscala/datastructures.scala
+++ src/main/scala/fpscala/datastructures.scala
@@ -1,5 +1,7 @@
 package fpscala.datastructures
 
+import scala.annotation.tailrec
+
 // singly-linked lists
 
 // the + annotation on the type parameter A indicates that A is a covariant parameter of List.
@@ -27,6 +29,53 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+  // exercise 3.2 - works properly, but the choice to return Nil wasn't recommended by the book
+  // I chose to do it because we're talking about purely functional data structures and signaling
+  // an error (in the way listed) is still a side effect.
+  def tail[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(_, Nil) => Nil
+      case Cons(_, xs) => xs
+    }
+  }
+  // exercise 3.3 - unclear if it's a good idea for the setHead function to modify empty lists
+  // once again, the book did it differently than I did and signaled an error. I modified my solution
+  // to be consistent with the tail fn
+  def setHead[A](l: List[A], h: A): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(_, xs) => Cons(h, xs)
+    }
+  }
+
+  // exercise 3.4 - is using ENSIME's type hinter cheating? (answered correctly)
+  // I didn't need as much pattern matching as I used here, but it was kinda fun to match on n
+  // while still easily getting drop into the correct tail position
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => n match {
+        case 0 => l
+        case _ => drop(t, n - 1)
+      }
+    }
+  }
+
+  // exercise 3.5 (answered properly)
+  // I didn't get the tail-recursive implementation on the first try.
+  // looking at the simpler solution given in the text's answers clued me in to the possibility
+  // of putting the call to dropWhile in the tail position by applying a logical not to f in the
+  // second to last case statement
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(h, t) if !f(h) => l
+      case Cons(_, t) => dropWhile(t, f)
+    }
+  }
 }
 
 object ListExamples {
@@ -46,5 +95,5 @@ object ex3pt1 {
     case Cons(h, t) => h + List.sum(t)
     case _ => 101
   }
-  require(x == 3) 
+  require(x == 3)
 }
