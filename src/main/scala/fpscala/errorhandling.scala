35d312b14ba10a1fc8561c981a3bd378ed3b5629
Andrew Foltz-Morrison <andrew.foltz-morrison@sitomobile.com>
2018-12-10 17:26:17 -0500

    beginning of Either, incorrect answer to 4.6

--- src/main/scala/fpscala/errorhandling.scala
+++ src/main/scala/fpscala/errorhandling.scala
@@ -1,4 +1,4 @@
-// package fpscala.errorhandling
+package fpscala.errorhandling
 
 import scala.{Option => _, Either => _, _}
 
@@ -92,4 +92,46 @@ object ExceptionExamples {
 
     def seqFromTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)((a) => a)
   }
+
+  // Scala implements disjoint union types through the Either type - an idea with
+  // broader applications that just error handling
+
+  // exercise 4.6 (answered incorrectly)
+  // once again I'm doing lots of pattern matching. it's hard for me to understand why this
+  // isn't the correct way to do it, as the textbook doesn't really offer reasons why it's
+  // more idiomatic (apart from maintainability, perhaps)
+  sealed trait Either[+E, +A] {
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Left(e)  => Left(e)
+      case Right(a) => Right(f(a))
+    }
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Right(a) => f(a)
+      case Left(e) => Left(e)
+    }
+    def orElse[EE >: E, B >: A](b: Either[EE, B]): Either[EE, B] = this match {
+      case Left(e) => Left(e)
+      case Right(b) => Right(b)
+    }
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = (this, b) match {
+      case (_, Left(e)) => Left(e)
+      case (Left(e), _) => Left(e)
+      case (Right(aa), Right(bb)) => Right(f(aa, bb))
+    }
+  }
+  case class Left[+E](value: E) extends Either[E, Nothing]
+  case class Right[+A](value: A) extends Either[Nothing, A]
+
+  def meanEither(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty) Left("mean of empty list")
+    else Right(xs.sum/xs.length)
+
+  // using either to encapsulate the error caused by the code
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x/y)
+    catch {case e: Exception => Left(e)}
+
+  def TryEither[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch {case e: Exception => Left(e)}
 }
