e0b4a75b4011ff5714e89bcc9668023eb88a15e1
Andrew Foltz-Morrison <andrew.foltz-morrison@sitomobile.com>
2018-12-10 12:16:04 -0500

    A partially incorrect answer to exercise 4.3

--- src/main/scala/fpscala/errorhandling.scala
+++ src/main/scala/fpscala/errorhandling.scala
@@ -47,5 +47,31 @@ object ExceptionExamples {
   // on most objects and classes made me forget that in the context of this chapter, it's
   // just a method on the Option type defined in these exercises.
   def variance(xs: Seq[Double]): Option[Double] =
-    mean(xs).flatMap(m => mean(xs.map(Math.pow(x - m, 2))))
+    mean(xs).flatMap(m => mean(xs.map{x => Math.pow(x - m, 2)}))
+
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _.map(f)
+
+  // def parseInsuranceRateQuote(
+  //   age: String,
+  //   numberOfSpeedingTickets: String
+  // ): Option[Double] = {
+  //   val optAge: Option[Int] = Try(age.toInt)
+  //   val optTickets: Option[Int] = Try(numberOfSpeedingTickets.toInt)
+
+  //   insuranceRateQuote(optAge, optTickets)
+  // }
+
+  def Try[A](a: => A): Option[A] =
+    try Some(a)
+    catch {case e: Exception => None}
+
+  // Exercise 4.3 (works, but answered incorrectly)
+  // was there a way to do this in terms of map or flatmap?
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    (a, b) match {
+      case (_, None) => None
+      case (None, _) => None
+      case (Some(x), Some(y)) => Some(f(x,y))
+    }
+  }
 }
